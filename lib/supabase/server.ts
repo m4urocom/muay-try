428142a4fe856d7f842a61fdea6b2b1720c8ff26
Mauro Arancibia <m4uro@Mauros-MacBook-Pro.local>
2025-05-01 17:50:21 +0700

    Update server.ts with cookie handling

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,23 +2,20 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createClient = () => {
-  const cookieStore = cookies()
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          const cookieStore = cookies()
+          return cookieStore.get(name)?.value ?? ''
         },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         set(name: string, value: string, options: CookieOptions) {
-          // The cookie setting will be handled by the middleware
+          // Cookie setting is handled by middleware
         },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         remove(name: string, options: CookieOptions) {
-          // The cookie removal will be handled by the middleware
+          // Cookie removal is handled by middleware
         },
       },
     }
