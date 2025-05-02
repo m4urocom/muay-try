e9c2732fc978549843cf4ad389ff49d3473c386e
Mauro Arancibia <m4uro@Mauros-MacBook-Pro.local>
2025-05-02 19:59:00 +0700

    fix: perfectly center login form on all screen sizes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,8 +10,8 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center w-full">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r w-1/2 max-w-xl">
         <div className="absolute inset-0 bg-sky-950" />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <Link href="/" className="flex items-center">
@@ -26,7 +26,7 @@ export default function LoginPage() {
           </blockquote>
         </div>
       </div>
-      <div className="w-full px-4 lg:p-8">
+      <div className="flex w-full items-center justify-center px-4 lg:w-1/2">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
